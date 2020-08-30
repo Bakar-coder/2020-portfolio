5c41d793d1a20ed8a8f97e4871964f277a4df04d
wabomba-bakar <wabombabakar@gmail.com>
2020-08-30 13:33:51 +0300

    cleaned app...

--- middleware/admin.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(req, res, next) {
-  if (!req.user.admin) return res.redirect("/users/login");
-  next();
-};
--- utils/database.js
+++
@@ -1,7 +0,0 @@
-const db = function() {
-  const localDb = "mongodb://localhost:27017/bassline";
-  const productionDb = "";
-  return process.env.NODE_ENV === "production" ? productionDb : localDb;
-};
-
-module.exports = db;
